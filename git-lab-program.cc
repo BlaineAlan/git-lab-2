bdf1473488e97812b180a341572b3c4921dd1ee0
BlaineAlan <156828846+BlaineAlan@users.noreply.github.com>
2024-01-23 11:58:54 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Blaine Cochensparger
+ *        Date: 1/23/2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+  cout << "Hello Git!!" << end1;
+    /*add code*/
+    return 0;
+}// main
